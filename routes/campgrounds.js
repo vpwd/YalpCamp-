9369ab16f4c8045b236f8981450727b8e03428e7
Vikas Patil <vikaspatil302@gmail.com>
2021-06-28 11:12:02 +0530

    last commit

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -48,8 +48,10 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
 router.get("/:id", function(req, res){
     //find the campground with provided ID
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if(err){
-            console.log(err);
+        if(err || !foundCampground){
+			req.flash("error", "Campground not found");
+            res.redirect("back");
+            //console.log(err);
         } else {
             console.log(foundCampground)
             //render show template with that campground
